fc1688e20f90fd096ac38fb2e4323a0b74748d87
Chenkaiwei <1035759181@qq.com>
2020-11-26 21:23:20 +0800

    Chore: 环境变量配置&API服务随环境自动改变

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
+const BASE_URL = process.env.REACT_APP_URL
 
 const instance = axios.create({
-	baseURL: 'https://api.kaikaio.com/api',
-	// baseURL: 'http://localhost:4000/api'
+	baseURL: BASE_URL
 });
 
 instance.interceptors.request.use((config) => {
+	// config.headers['Authorization'] = 'Bearer ' + localStorage.token
 	return config;
 },  (error) => {
 	console.error(error)
