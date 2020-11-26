079378718ad306ac2ae39ceb284a702916d0c4f8
Chenkaiwei <1035759181@qq.com>
2020-11-26 16:33:19 +0800

    Fix: API服务路径配置错误缺陷修复

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'https://api.kaikaio.com',
+	baseURL: 'https://api.kaikaio.com/api',
 	// baseURL: 'http://localhost:4000/api'
 });
 
